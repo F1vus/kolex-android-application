394910977dd36d57ec236aba43c1c609a46aff78
Fiv_ <nzaikar184@gmail.com>
2026-05-05 19:26:46 +0200

    create new activities

--- app/src/main/java/edu/at/kolex/fragment/TicketsFragment.java
+++ app/src/main/java/edu/at/kolex/fragment/TicketsFragment.java
@@ -8,11 +8,22 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.at.kolex.adapter.TicketAdapter;
 import edu.at.kolex.databinding.FragmentTicketsBinding;
+import edu.at.kolex.model.Ticket;
+import edu.at.kolex.viewmodel.TicketsViewModel;
 
-public class TicketsFragment extends Fragment {
+public class TicketsFragment extends Fragment implements TicketAdapter.OnTicketClickListener {
     private FragmentTicketsBinding binding;
+    private TicketAdapter ticketAdapter;
+    private TicketsViewModel viewModel;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +34,62 @@ public class TicketsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initializeViewModel();
+        initializeRecyclerView();
+        observeViewModel();
+        loadTickets();
+    }
+
+    private void initializeViewModel() {
+        viewModel = new ViewModelProvider(this).get(TicketsViewModel.class);
+    }
+
+    private void initializeRecyclerView() {
+        ticketAdapter = new TicketAdapter(new ArrayList<>(), this);
+        binding.rvTickets.setLayoutManager(new LinearLayoutManager(requireContext()));
+        binding.rvTickets.setAdapter(ticketAdapter);
+    }
+
+    private void observeViewModel() {
+        viewModel.getTickets().observe(getViewLifecycleOwner(), tickets -> {
+            if (tickets != null) {
+                updateUI(tickets);
+            }
+        });
+
+        viewModel.getIsLoading().observe(getViewLifecycleOwner(), isLoading -> {
+            binding.progressBar.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+        });
+
+        viewModel.getError().observe(getViewLifecycleOwner(), error -> {
+            if (error != null && !error.isEmpty()) {
+                // Handle error
+            }
+        });
+    }
+
+    private void updateUI(List<Ticket> tickets) {
+        if (tickets.isEmpty()) {
+            binding.rvTickets.setVisibility(View.GONE);
+            binding.emptyStateLayout.setVisibility(View.VISIBLE);
+        } else {
+            binding.rvTickets.setVisibility(View.VISIBLE);
+            binding.emptyStateLayout.setVisibility(View.GONE);
+            ticketAdapter.updateTickets(tickets);
+        }
+    }
+
+    private void loadTickets() {
+        viewModel.loadUserTickets();
+    }
+
+    @Override
+    public void onTicketClick(Ticket ticket) {
+        // Action on ticket click
+    }
+
+    public void refreshTickets() {
+        loadTickets();
     }
 
     @Override
